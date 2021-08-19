44206ccf73a4c6d0af87eb1dac25a1eff71a75a0
Abraham Vong <73206726+AbrahamTemple@users.noreply.github.com>
2021-08-19 12:07:21 +0800

    Update Banner.vue

--- vue-skitter/src/components/Banner.vue
+++ vue-skitter/src/components/Banner.vue
@@ -1,10 +1,10 @@
 <template>
      <div class="box_skitter box_skitter_large" ref="skitters">
             <ul>
-                <li v-for="(img,$index) in banner" :key="$index">
-                    <a :href="img.link" target="_blank"><img :src="img.url" :class="img.mode"/></a>
+                <li v-for="(item,$index) in banner" :key="$index">
+                    <a :href="item.link" target="_blank"><img :src="item.url" :class="item.mode"/></a>
                     <div class="label_text">
-                        <p><strong><font :color="img.color">{{img.title}}</font></strong></p>
+                        <p><strong><font :color="item.color">{{item.title}}</font></strong></p>
                     </div>
                 </li>
             </ul>
@@ -23,21 +23,21 @@
                     {
                         url: require('@/assets/img/001.jpg'),
                         mode: 'circlesRotate',
-                        link: '#list/41',
+                        link: '#list/1',
                         title: '繁华落幕的城市',
                         color: '#ffffff'
                     },
                     {
                         url: require('@/assets/img/002.jpg'),
                         mode: 'cubeRandom',
-                        link: '#list/19',
+                        link: '#list/2',
                         title: '孤舟独进的邮轮',
                         color: '#FF00FF'
                     },
                     {
                         url: require('@/assets/img/003.jpg'),
                         mode: 'horizontal',
-                        link: '#list/48',
+                        link: '#list/3',
                         title: '照亮黑夜的极光',
                         color: '#FFFF00'
                     }
